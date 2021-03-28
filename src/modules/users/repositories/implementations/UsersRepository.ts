72b238482dd0193d80e95933b61bbf3622a1071f
Rafael Candido <rafarc19@gmail.com>
2021-03-28 14:38:50 -0300

    28/03 14:37 Montando a api e realizando os testes

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,41 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+      created_at: new Date(),
+    });
+
+    this.users.push(user);
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    const User = this.users.find((user) => user.id === id);
+    return User;
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    const User = this.users.find((user) => user.email === email);
+    return User;
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const index = this.users.indexOf(receivedUser);
+    const updated = {
+      admin: true,
+      updated_at: new Date(),
+    };
+    const user = { ...receivedUser, ...updated };
+    this.users[index] = user;
+    return user;
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,15 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const requestHeader = JSON.stringify(request.headers);
+    const { user_id } = JSON.parse(requestHeader);
+
+    try {
+      const users = this.listAllUsersUseCase.execute({ user_id });
+      return response.json(users);
+    } catch (error) {
+      return response.status(400).json({ error });
+    }
   }
 }
 
